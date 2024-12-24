bce4613c6697e3ef76307492f8aed778508689fc
KeiAzDev <enopiyo3code+work@gmail.com>
2024-12-24 15:34:12 -0800

    パスワードの暗号化と新規作成追加

--- server/index.js
+++ server/index.js
@@ -1,7 +1,41 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+import CryptoJS from 'crypto-js';
+import User from './src/v1/models/user.js';
+import env from 'dotenv';
+env.config();
+
 
 const app = express();
 const PORT = 8080;
+
+//DB connect
+try {
+  mongoose.connect(process.env.MONGODB_URL);
+  console.log('DBと接続中');
+} catch(err) {
+  console.log(err);
+}
+
+//register
+app.post('/register', async (req, res) => {
+  const password = req.body.password;
+
+  try {
+    //パスワードの暗号化
+    req.body.password = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY);
+    //ユーザーの新規作成
+    const user = await new User.create(req.body);
+    //jwt
+
+  } catch {
+
+  }
+})
+
+//login
+
+
 
 app.listen(PORT, () => {
   console.log('サーバー起動中');
