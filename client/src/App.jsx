641eee038e75a61fb18f02e4e8a26a07da58ef5a
KeiAzDev <enopiyo3code+work@gmail.com>
2024-12-26 13:24:12 -0800

    AuthLayoutにloginとregisterをリンクしました

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css'
+import AuthLayout from './components/layout/AuthLayout'
+import Login from './pages/Login'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Register from './pages/Register'
 
 function App() {
 
 
   return (
-    <>
-      asadfads
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<AuthLayout />}>
+          <Route path='login' element={<Login />}/>
+          <Route path='Register' element={<Register />}/>
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
