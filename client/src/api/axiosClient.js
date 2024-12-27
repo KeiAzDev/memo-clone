fcff54beb0176efb938ade073e2f479faf20be5f
KeiAzDev <enopiyo3code+work@gmail.com>
2024-12-27 15:01:52 -0800

    axios interceptors 追加

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,28 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/api/v1'
+const BASE_URL = 'http://localhost:8080/api/v1';
+const getToken = () => localStorage.getItem('token');
 
 const axiosClient = axios.create({
   baseURL: BASE_URL,
-})
+});
+
+//api前処理
+axiosClient.interceptors.request.use(async(config) => {
+  return {
+    config,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${getToken()}`,//リクエストヘッダにJWTをつけてサーバーに渡す
+    },
+  };
+});
+
+axiosClient.interceptors.response.use((response) => {
+  return response;
+}, (err) => {
+  throw err.response;
+}
+);
+
+export default axiosClient;
