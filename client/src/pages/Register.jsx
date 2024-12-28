27ed9581f1a11fedadd0b53ee812444af3eed453
KeiAzDev <enopiyo3code+work@gmail.com>
2024-12-27 18:54:34 -0800

    register validation 追加

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box, Button, TextField } from '@mui/material'
 import { LoadingButton } from '@mui/lab'
 import { Link } from 'react-router-dom'
@@ -6,8 +6,16 @@ import authApi from '../api/authApi'
 
 const Register = () => {
 
+  const [usernameErrText, setUsernameErrText] = useState('');
+  const [passwordErrText, setPasswordErrText] = useState('');
+  const [confirmPasswordErrText, setConfirmPasswordErrText] = useState('');
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setUsernameErrText('');
+    setPasswordErrText('');
+    setConfirmPasswordErrText('');
+
     //入力欄の文字列を取得する
     const data = new FormData(e.target);
     const username = data.get('username').trim();
@@ -16,6 +24,27 @@ const Register = () => {
     console.log(username);
     console.log(password);
     console.log(confirmPassword);
+
+    let error = false;
+
+    if(username === '') {
+      error = true;
+      setUsernameErrText('名前を入力してください');
+    }
+    if(password === '') {
+      error = true;
+      setPasswordErrText('パスワードを入力してください');
+    }
+    if(confirmPassword === '') {
+      error = true;
+      setConfirmPasswordErrText('確認用パスワードを入力してください');
+    }
+    if(password !== confirmPassword) {
+      error = true;
+      setConfirmPasswordErrText('パスワードと確認用パスワードが異なります');
+    }
+
+    if(error) return;
 
     //新規登録APIを叩く
     try {
@@ -29,10 +58,10 @@ const Register = () => {
 
   return (
     <>
-      <Box component='form' onSubmit={handleSubmit}>
-        <TextField fullWidth id='username' label='お名前' margin='normal' name='username' required />
-        <TextField fullWidth id='password' label='パスワード' margin='normal' name='password' type='password' required />
-        <TextField fullWidth id='confirmPassword' label='確認用パスワード' margin='normal' name='confirmPassword' type='password' required />
+      <Box component='form' onSubmit={handleSubmit} noValidate>
+        <TextField fullWidth id='username' label='お名前' margin='normal' name='username' required helperText={usernameErrText} />
+        <TextField fullWidth id='password' label='パスワード' margin='normal' name='password' type='password' required helperText={passwordErrText} />
+        <TextField fullWidth id='confirmPassword' label='確認用パスワード' margin='normal' name='confirmPassword' type='password' required helperText={confirmPasswordErrText} />
         <LoadingButton sx={{mt: 3, mb: 2}} fullWidth type='submit' loading={false} color='primaey' variant='outlined' >アカウント作成</LoadingButton>
       </Box>
         <Button component={Link} to='/login' >すでにアカウントをお持ちですか？ログイン</Button>
