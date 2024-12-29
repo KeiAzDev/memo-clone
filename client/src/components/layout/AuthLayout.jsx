c9c9300b2ee77e5ef0e5bc1e2177b0337cbabb5a
KeiAzDev <enopiyo3code+work@gmail.com>
2024-12-29 11:56:16 -0800

    ログイン後のトークン判定、認証チェック実装

--- client/src/components/layout/AuthLayout.jsx
+++ client/src/components/layout/AuthLayout.jsx
@@ -1,9 +1,25 @@
 import { Box, Container } from '@mui/material'
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 import memoLogo from '../../assets/images/logo.png'
+import authUtils from '../../utils/authUtils'
 
 const AuthLayout = () => {
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    //jwtを持っているか
+    const checkAuth = async() => {
+      //認証チェック
+      const isAuth = await authUtils.isAuthenticated();
+      if(isAuth) {
+        navigate('/');
+      }
+    };
+    checkAuth();
+  },[navigate]);
+
   return (
     <div>
       <Container component='main' maxWidth='xs'>
