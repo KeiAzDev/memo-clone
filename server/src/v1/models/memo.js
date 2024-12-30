dc987f1709bc3fbe6b900b90f7e5c419e63f4619
KeiAzDev <enopiyo3code+work@gmail.com>
2024-12-29 19:26:36 -0800

    memoSchema追加

---
+++ server/src/v1/models/memo.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const memoSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  icon: {
+    type: String,
+    default: '📝',
+  },
+  title: {
+    type: String,
+    default: '無題',
+  },
+  description: {
+    type: String,
+    default: 'ここになんでも書いてね',
+  },
+  position: {
+    type: Number,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+  favoritePosition: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Memo = mongoose.model('Memo', userSchema);
+
+export default Memo;
