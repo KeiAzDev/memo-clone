4c05c9e525d77cfc1cbdd929640b8e8ffe9d59dd
KeiAzDev <enopiyo3code+work@gmail.com>
2024-12-30 12:05:23 -0800

    home component 追加

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box } from '@mui/material'
+import { LoadingButton } from '@mui/lab'
+
 
 const Home = () => {
+  const [loading, setLoading] = useState(false); 
+
+  const createMemo = () => {
+
+  }
+
   return (
-    <div>
-      home
-    </div>
+    <Box sx={{height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center',}}>
+      <LoadingButton variant='outlined' color='success' onClick={()=> createMemo} loading={loading}>
+        最初のメモを作成
+      </LoadingButton>
+    </Box>
   )
 }
 
