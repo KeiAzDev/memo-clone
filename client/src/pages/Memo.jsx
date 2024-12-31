ed3ac158f2add6f1d85c71b34c838454244efd07
KeiAzDev <enopiyo3code+work@gmail.com>
2024-12-31 11:51:12 -0800

    メモ内容をmongoDBへと保存するApi追加

--- client/src/pages/Memo.jsx
+++ client/src/pages/Memo.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Box, IconButton, TextField} from '@mui/material'
 import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined'
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined'
+import { useParams } from 'react-router-dom'
+import memoApi from '../api/memoApi'
 
 const Memo = () => {
+  const {memoId} = useParams();
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  useEffect(() => {
+    const getMemo = async() => {
+      try {
+        const res = await memoApi.getOne(memoId);
+        console.log(res);
+        setTitle(res.title);
+        setDescription(res.description);
+      } catch(err) {
+        alert(err);
+      }
+    };
+    getMemo();
+  },[memoId]);
+
+  let timer;
+  const timeout = 500;
+
+  const updateTitle = async (e) => {
+    clearTimeout(timer);
+    const newTitle = e.target.value;
+    setTitle(newTitle);
+
+    timer = setTimeout(async () =>{
+      try {
+        await memoApi.update(memoId, {title: newTitle});
+      } catch (err) {
+        alert(err);
+      }
+    }, timeout);
+  };
+
+  const updateDescription = async (e) => {
+    clearTimeout(timer);
+    const newDescription = e.target.value;
+    setDescription(newDescription);
+
+    timer = setTimeout(async () =>{
+      try {
+        await memoApi.update(memoId, {description: newDescription});
+      } catch (err) {
+        alert(err);
+      }
+    }, timeout);
+  };
+
   return (
     <>
       <Box sx={{display: 'flex', alignItems: 'center', width: '100%' }}>
@@ -15,8 +66,8 @@ const Memo = () => {
         </IconButton>
       </Box>
       <Box sx={{p: '10px 50px'}}>
-        <TextField placeholder='無題' variant='outlined' fullWidth sx={{'.MuiOutlinedInput-input': {padding: 0}, '.MuiOutlinedInput-notchedOutline': {border: 'none'}, '.MuiOutlinedInput-root': {fontSize: '2rem', fontWeight: '700'}}}/>
-        <TextField placeholder='追加' variant='outlined' fullWidth sx={{'.MuiOutlinedInput-input': {padding: 0}, '.MuiOutlinedInput-notchedOutline': {border: 'none'}, '.MuiOutlinedInput-root': {fontSize: '1rem'}}}/>
+        <TextField onChange={updateTitle} value={title} placeholder='無題' variant='outlined' fullWidth sx={{'.MuiOutlinedInput-input': {padding: 0}, '.MuiOutlinedInput-notchedOutline': {border: 'none'}, '.MuiOutlinedInput-root': {fontSize: '2rem', fontWeight: '700'}}}/>
+        <TextField onChange={updateDescription} value={description} placeholder='追加' variant='outlined' fullWidth sx={{'.MuiOutlinedInput-input': {padding: 0}, '.MuiOutlinedInput-notchedOutline': {border: 'none'}, '.MuiOutlinedInput-root': {fontSize: '1rem'}}}/>
       </Box>
     </>
   )
--- server/src/v1/routes/memo.js
+++ server/src/v1/routes/memo.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { create, getAllMemo, getOne } from '../controllers/memo.js';
+import { create, getAllMemo, getOne, update } from '../controllers/memo.js';
 import { verifyToken } from '../handlers/tokenHandler.js';
 
 const router = Router();
@@ -12,5 +12,8 @@ router.get('/', verifyToken, getAllMemo);
 
 //ログインしているユーザーが投稿したメモを一つ取得
 router.get('/:memoId', verifyToken, getOne);
+
+//ログインしているユーザーが投稿したメモを更新
+router.put('/:memoId', verifyToken, update);
 
 export default router;
