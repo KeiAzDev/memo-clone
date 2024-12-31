4e270213615374b8b8d1de2152ae2a2c663242cf
KeiAzDev <enopiyo3code+work@gmail.com>
2024-12-31 10:03:31 -0800

    メモjsxにメモ欄追加

--- client/src/pages/Memo.jsx
+++ client/src/pages/Memo.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import {Box, IconButton, TextField} from '@mui/material'
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined'
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined'
 
 const Memo = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%' }}>
+        <IconButton>
+          <StarBorderOutlinedIcon />
+        </IconButton>
+        <IconButton variant='outlined' color='error'>
+          <DeleteOutlinedIcon />
+        </IconButton>
+      </Box>
+      <Box sx={{p: '10px 50px'}}>
+        <TextField placeholder='無題' variant='outlined' fullWidth />
+        <TextField placeholder='追加' variant='outlined' fullWidth />
+      </Box>
+    </>
   )
 }
 
