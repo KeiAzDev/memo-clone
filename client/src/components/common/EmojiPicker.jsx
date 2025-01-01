1d6eac0a377d5b051b74db152efc936530455917
KeiAzDev <enopiyo3code+work@gmail.com>
2025-01-01 12:07:13 -0800

    emojiPicker追加

--- client/src/components/common/EmojiPicker.jsx
+++ client/src/components/common/EmojiPicker.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import { Box, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import Picker from '@emoji-mart/react'
 
 const EmojiPicker = (props) => {
+  const [selectedEmoji, setSelectedEmoji] = useState();
+  const [isShowPicker, setIsShowPicker] = useState(false);
+
+  useEffect(() => {
+    setSelectedEmoji(props.icon);
+  }, [props.icon]);
+
+  const showPicker = () => setIsShowPicker(!isShowPicker);
+
+  const selectEmoji = (e) => {
+    const emojiCode = e.unified.split('-');
+    let codesArray = [];
+    emojiCode.forEach((el) => codesArray.push('0x' + el));
+    const emoji = String.fromCodePoint(...codesArray);
+    setIsShowPicker(false);
+  };
+
   return (
-    <div>
-      {props.icon}
-    </div>
+    <Box>
+      <Typography variant='h3' fontWeight={700} sx={{ cursor: 'pointer' }} onClick={showPicker}>
+        {selectedEmoji}
+      </Typography>
+      <Box sx={{ display: isShowPicker ? 'block' : 'none', position: 'absolute', zIndex: '100' }}>
+        <Picker onEmojiSelect={selectEmoji}/>
+      </Box>
+    </Box>
   )
 }
 
